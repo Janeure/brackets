a8026f6f461cadbb6043e980fadb43b48941ed5a
Евгения Рогозова <lalwat@mail.ru>
2022-07-25 20:07:38 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let box = {};
+  let open = []
+  for (key of bracketsConfig) {
+    box[key[1]] =  key[0];
+    open.push(key[0])
+}
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let top = stack[stack.length-1];
+    let x = str[i];
+    if (stack.length !== 0 && box[x] === top) {
+          stack.pop();
+    }
+    else {
+      stack.push(x);
+    }
+  }
+  return stack.length === 0;
 }
